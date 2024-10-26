57707fa4eac9088a38564fb7f2b41f7503218c7a
Thiane <thianedepra@hotmail.com>
2024-10-25 21:34:35 -0300

    refactor: acrescentando observacoes

--- java-jre-jdk-i/src/TestaVariaveis1.java
+++ java-jre-jdk-i/src/TestaVariaveis1.java
@@ -1,20 +1,21 @@
-public class TestaInteiro {
+public class TestaVariaveis1 {
 
     public static void main(String[] args) {
-
         System.out.println("Olá, novo teste!");
 
         int idade;
-        idade = 37; //atribuição de um valor
+        // = é atribuição  de um valor
+        idade = 37;
         System.out.println(idade);
 
-        //Operações aritméticas
+        //operações aritméticas
         idade = 30 + 10;
         System.out.println(idade);
 
         idade = (7 * 5) + 2;
         System.out.println(idade);
 
+        // + é concatenação alguma coisa com uma string
         System.out.println("A idade é " + idade);
     }
 
