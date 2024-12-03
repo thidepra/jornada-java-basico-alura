ea828dbf71e2939ffc9e8ec00818f581756260ab
Thiane <thianedepra@hotmail.com>
2024-12-02 21:02:11 -0300

    refactor: conclusão do curso 2

--- java-oo-ii/bytebank/src/Conta.java
+++ java-oo-ii/bytebank/src/Conta.java
@@ -2,9 +2,25 @@ public class Conta {
     private double saldo;
     private int agencia;
     private int numero;
-    Cliente titular;
+    private Cliente titular;
+    //static é um atributo da classe e não do objeto
+    private static int total;
 
+    //construtor não é metodo, uma rotina de inicialização
     public Conta() {
+        //não é o ideal para uma conta ter o construtor padrão, mas ficará assim para os exemplos iniciais
+    }
+
+    public Conta(int agencia, int numero) {
+        if (agencia <= 0 || numero <= 0) {
+            System.out.println("Não pode valor menor ou igual a 0");
+            return;
+        }
+        //this é uma referência ao objeto que está sendo instanciado
+        this.agencia = agencia;
+        this.numero = numero;
+//        Conta.total++;
+        total++;
     }
 
     public double getSaldo() {
@@ -15,7 +31,12 @@ public class Conta {
         return this.agencia;
     }
 
+    //não precisaria desses métodos set se tivesse apenas o construtor: public Conta(int agencia, int numero)
     public void setAgencia(int agencia) {
+        if (agencia <= 0) {
+            System.out.println("Não pode valor menor ou igual a 0");
+            return;
+        }
         this.agencia = agencia;
     }
 
@@ -24,6 +45,10 @@ public class Conta {
     }
 
     public void setNumero(int numero) {
+        if (numero <= 0) {
+            System.out.println("Não pode valor menor ou igual a 0");
+            return;
+        }
         this.numero = numero;
     }
 
@@ -33,6 +58,10 @@ public class Conta {
 
     public void setTitular(Cliente titular) {
         this.titular = titular;
+    }
+
+    public static int getTotal() {
+        return total;
     }
 
     //metodo void não retorna nenhum valor mas pode alterar o estado do objeto, ou seja, alterar os valores dos atributos
--- java-oo-ii/bytebank/src/TestaValores.java
+++ java-oo-ii/bytebank/src/TestaValores.java
@@ -1,15 +1,23 @@
 public class TestaValores {
 
     public static void main(String[] args) {
-        Conta conta = new Conta(1337, 24226);//os parentes invocam o construtor
+        //os parentes invocam o construtor
+        Conta conta = new Conta(1337, 24226);
 
+        //conta inconsistente com a regra de negócio
+//        conta.setAgencia(-50);
+//        conta.setNumero(-330);
+        //imprimi 0, pois não foi atribuído nenhum valor e pega o valor padrão
         System.out.println(conta.getAgencia());
-        conta.setAgencia(123443); //chama a referência
+
+        //chama a referência
+        conta.setAgencia(123443);
 
         Conta conta2 = new Conta(1337, 16549);
         Conta conta3 = new Conta(1337, 14660);
 
-        System.out.println(Conta.getTotal());//preciso chamar a classe toda e não o objeto conta 1 por exemplo
+        //preciso chamar a classe toda e não o objeto conta 1 por exemplo
+        System.out.println(Conta.getTotal());
     }
 
 }
